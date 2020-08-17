d1f5d709d7c45bb7756d3e747700f087f68bbed3
MisayaQ <516802581@qq.com>
2020-08-17 20:55:59 +0800

    ‘Josepful 双向链表’

---
+++ src/com/atguigu/sparsearray/Test.java
@@ -0,0 +1,35 @@
+package com.atguigu.sparsearray;
+
+/**
+ * @program: dataStructure
+ * @description:
+ * @version: 1.0
+ * @author: LiuJiaQi
+ * @create: 2020-08-14 12:27
+ **/
+public class Test {
+    public static volatile int count = 0;
+
+    public static void increase() {
+        count++;
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i1 = 0; i1 < 1000; i1++) {
+                        increase();
+                    }
+                }
+            });
+            threads[i].start();
+        }
+        while (Thread.activeCount() > 1) {
+            Thread.yield();
+        }
+        System.out.println(count);
+    }
+}
